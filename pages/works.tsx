ccd9b1e47c4b994b068dce6bf2b929ea1c9475e9
Paul Lorenc <pjlorenc@gmail.com>
2020-12-30 18:36:29 -0500

    fixed page margins

--- pages/works.tsx
+++ pages/works.tsx
@@ -2,7 +2,7 @@ import { Container } from "@/components/Container";
 export default function Works() {
   return (
     <Container>
-      <div className="font-serif text-2xl font-extrabold items-center max-w-2xl">
+      <div className="mt-20 font-serif text-2xl font-extrabold items-center max-w-2xl">
         works
       </div>
     </Container>
