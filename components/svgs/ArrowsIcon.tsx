5a9044df28d8ae1cd8dced9af911420add9878c6
Paul Lorenc <pjlorenc@gmail.com>
2020-12-31 12:31:39 -0500

    added arrow icon

---
+++ components/svgs/ArrowsIcon.tsx
@@ -0,0 +1,122 @@
+const ArrowsIcon = (props: any) => (
+  <svg
+    viewBox="0 0 160 40"
+    preserveAspectRatio="none"
+    className="w-full h-10 py-2"
+  >
+    <path
+      d="M161.8,40h1.4l8-20h-2.9Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "currentColor",
+      }}
+    />
+    <path
+      d="M164.7,40l6.5-20-8,20Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "#a7a9ac",
+      }}
+    />
+    <path
+      d="M141.1,20H137L117.5,40h2.1Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "currentColor",
+      }}
+    />
+    <path
+      d="M121.7,40l19.4-20L119.6,40Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "#a7a9ac",
+      }}
+    />
+    <path
+      d="M106.9,20H102L71.7,40h2.6Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "currentColor",
+      }}
+    />
+    <path
+      d="M106.9,20,74.3,40h2.3Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "#a7a9ac",
+      }}
+    />
+    <path
+      d="M68.3,20,23,40h2.8Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "#a7a9ac",
+      }}
+    />
+    <path
+      d="M61.8,20,19.3,40H23L68.3,20Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "currentColor",
+      }}
+    />
+    <path
+      d="M161.8,0h1.4l8,20h-2.9Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "currentColor",
+      }}
+    />
+    <path
+      d="M164.7,0l6.5,20-8-20Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "#a7a9ac",
+      }}
+    />
+    <path
+      d="M141.1,20H137L117.5,0h2.1Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "currentColor",
+      }}
+    />
+    <path
+      d="M121.7,0l19.4,20L119.6,0Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "#a7a9ac",
+      }}
+    />
+    <path
+      d="M106.9,20H102L71.7,0h2.6Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "currentColor",
+      }}
+    />
+    <path
+      d="M106.9,20,74.3,0h2.3Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "#a7a9ac",
+      }}
+    />
+    <path
+      d="M68.3,20,23,0h2.8Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "#a7a9ac",
+      }}
+    />
+    <path
+      d="M61.8,20,19.3,0H23L68.3,20Z"
+      transform="translate(-19.3)"
+      style={{
+        fill: "currentColor",
+      }}
+    />
+  </svg>
+);
+
+export default ArrowsIcon;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import { Container } from "@/components/Container";
+import ArrowsIcon from "@/components/svgs/ArrowsIcon";
+import ZFIcon from "@/components/svgs/ZFIcon";
 import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
@@ -6,13 +8,15 @@ export default function Home() {
     <Container>
       <div className="font-serif md:h-screen w-screen px-4 text-black dark:text-white text-2xl font-extrabold mt-16 md:mt-20 max-w-3xl">
         <div className="flex flex-col-reverse md:flex-row items-center border border-black dark:border-white px-4 pb-1">
-          <div className="flex flex-col">
-            <div className="pt-7 pb-2 text-md">Your Idea Our Brush</div>
-            <p className="hidden md:block font-thin font-sm">
-              we will we will rock you
+          <div className="flex flex-col md:w-1/2 mt-1">
+            <div className="pt-7 pb-1 text-md">Your Idea Our Brush</div>
+            <p className="hidden md:block font-thin font-xs">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam,
             </p>
           </div>
-          <div className="relative w-64 md:w-3/5 h-auto max-w-sm md:max-w-lg max-h-80">
+          <div className="relative mt-2 mb-3 w-64 md:w-3/5 h-auto max-w-sm md:max-w-lg max-h-80">
             <Image
               src="/images/flower.png"
               width={500}
@@ -25,15 +29,21 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="flex flex-col items-stretch md:items-start mb-10">
+        <div className="flex flex-col font-base items-stretch w-full md:items-start">
           <Link href="/works">
-            <a className="flex items-start border border-black dark:border-white mt-5 px-4 py-2 pr-20 md:w-1/3">
-              {"Works    ->"}
+            <a className="flex items-center justify-between border border-black dark:border-white mt-5 px-4 py-2 md:w-1/3">
+              <a>{"Works"}</a>
+              <div className="pl-4 h-full w-2/3">
+                <ArrowsIcon />
+              </div>
             </a>
           </Link>
           <Link href="/about">
-            <a className="flex items-start border border-black dark:border-white mt-5 px-4 py-2 pr-20 md:w-1/3">
-              {"About    ->"}
+            <a className="flex items-center justify-between border border-black dark:border-white mt-5 px-4 py-2 md:w-1/3">
+              <a>{"About"}</a>
+              <div className="pl-4 h-full w-2/3">
+                <ArrowsIcon />
+              </div>
             </a>
           </Link>
         </div>
