3079424e217e7266d1d2dd7e5550673ba824fbbf
Paul Lorenc <pjlorenc@gmail.com>
2021-02-07 13:59:54 -0500

    home page clean up to share progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,25 +9,25 @@ export default function Home() {
   let flowerRef: any = useRef(null);
 
   useEffect(() => {
-    TweenMax.from(flowerRef, 1, { y: 50, opacity: 0, ease: "power2.out" });
+    TweenMax.from(flowerRef, 1.5, { y: 50, opacity: 0, ease: "power2.out" });
   }, []);
   return (
     <Container>
       <div className="z-1 text-black dark:text-white">
         <div
-          className="zf-switch grid grid-cols-1 md:grid-cols-2  h-screen bg-white dark:bg-black mt-20"
+          className="zf-switch grid grid-cols-1  h-screen bg-white dark:bg-black mt-20"
           ref={(el: any) => {
             flowerRef = el;
           }}
         >
-          <div className="flex justify-left md:mt-20">
+          {/* <div className="flex justify-left md:mt-20">
             <button className="bg-blue-200 dark:bg-blue-900 bg-opacity-60 px-3 py-1 text-xl font-bold border border-blue-50 dark:border-blue-900">
               Contact Sales
             </button>
             <button className="zf-card px-3 py-1 text-xl font-bold border">
               View Works
             </button>
-          </div>
+          </div> */}
           <div className="mt-20">
             <Image src="/images/flower.png" height={800} width={800} />
           </div>
