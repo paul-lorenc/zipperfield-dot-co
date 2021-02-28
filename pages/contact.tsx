10c87cff595880e5f0ee1f5334070fa878bc7fda
Paul Lorenc <pjlorenc@gmail.com>
2021-02-27 21:34:01 -0500

    old contact form cleanup

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,4 +1,3 @@
-import { Container } from "@/components/Container";
 import { useState } from "react";
 
 export default function Contact() {
@@ -60,53 +59,51 @@ export default function Contact() {
   };
 
   return (
-    <Container>
-      <main className="mt-16 md:mt-28 text-black dark:text-gray-200 w-1/2 h-screen px-6">
-        <form
-          className="flex flex-col space-y-4 items-center justify-center"
-          onSubmit={handleOnSubmit}
+    <main className="mt-16 md:mt-28 text-black dark:text-gray-200 w-1/2 h-screen px-6">
+      <form
+        className="flex flex-col space-y-4 items-center justify-center"
+        onSubmit={handleOnSubmit}
+      >
+        <label htmlFor="email" className="text-xl">
+          Email
+        </label>
+        <input
+          id="email"
+          type="email"
+          onChange={handleOnChange}
+          className="zf-card p-1.5 dark:focus:border-white focus:shadow-lg"
+          required
+          value={inputs.email}
+        />
+        <label htmlFor="message" className="text-xl">
+          Message
+        </label>
+        <textarea
+          id="message"
+          onChange={handleOnChange}
+          className="zf-card p-4 w-full md:w-2/3 dark:focus:border-white dark:focus:shadow-blue"
+          required
+          rows={10}
+          value={inputs.message}
+        />
+        <button
+          type="submit"
+          className="zf-card px-2 py-1 text"
+          disabled={status.submitting}
         >
-          <label htmlFor="email" className="text-xl">
-            Email
-          </label>
-          <input
-            id="email"
-            type="email"
-            onChange={handleOnChange}
-            className="zf-card p-1.5 dark:focus:border-white focus:shadow-lg"
-            required
-            value={inputs.email}
-          />
-          <label htmlFor="message" className="text-xl">
-            Message
-          </label>
-          <textarea
-            id="message"
-            onChange={handleOnChange}
-            className="zf-card p-4 w-full md:w-2/3 dark:focus:border-white dark:focus:shadow-blue"
-            required
-            rows={10}
-            value={inputs.message}
-          />
-          <button
-            type="submit"
-            className="zf-card px-2 py-1 text"
-            disabled={status.submitting}
-          >
-            {!status.submitting
-              ? !status.submitted
-                ? "Submit"
-                : "Submitted"
-              : "Submitting..."}
-          </button>
-        </form>
-        {status.info.error && (
-          <div className="error">Error: {status.info.msg}</div>
-        )}
-        {!status.info.error && status.info.msg && (
-          <div className="success">{status.info.msg}</div>
-        )}
-      </main>
-    </Container>
+          {!status.submitting
+            ? !status.submitted
+              ? "Submit"
+              : "Submitted"
+            : "Submitting..."}
+        </button>
+      </form>
+      {status.info.error && (
+        <div className="error">Error: {status.info.msg}</div>
+      )}
+      {!status.info.error && status.info.msg && (
+        <div className="success">{status.info.msg}</div>
+      )}
+    </main>
   );
 }
